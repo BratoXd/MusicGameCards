4f3fa949270c042697bf0d5dfdffda078e5a27b2
BraulioHernandez13 <brauliocesar.hr@gmail.com>
2024-05-09 03:44:58 -0600

    Se agregan estadisticas globales y se divide el mapa para que sean 2

---
+++ Assets/GlobalStats.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GlobalStats : MonoBehaviour
+{
+
+    public int EnsambleGlobalStat;
+    public int RitmoGlobalStat;
+    public int ArmoniasGlobalStat;
+    public int MelodiasGlobalStat;
+    public TextMeshProUGUI EnsambleGlobalStat_TMP;
+    public TextMeshProUGUI RitmoGlobalStat_TMP;
+    public TextMeshProUGUI ArmoniasGlobalStat_TMP;
+    public TextMeshProUGUI MelodiasGlobalStat_TMP;
+    // Start is called before the first frame update
+
+
+    void setGlobalStat()
+    {
+
+
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/Scripts/DropZone.cs
+++ Assets/Scripts/DropZone.cs
@@ -1,12 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UIElements;
 
 public class DropZone : MonoBehaviour, IDropHandler, IPointerClickHandler, IPointerExitHandler, IPointerEnterHandler
 {
-  public void OnDrop(PointerEventData eventData) { }
+
+  public GlobalStats globalStats;
+
+  public void OnDrop(PointerEventData eventData)
+  {
+    CardMovementController card = eventData.pointerDrag.GetComponent<CardMovementController>();
+
+ 
+
+
+    globalStats.EnsambleGlobalStat =  globalStats.EnsambleGlobalStat+ card.dataCard.DireccionScore;
+    globalStats.EnsambleGlobalStat_TMP.text = "Poder de Ensable: " + globalStats.EnsambleGlobalStat.ToString();
+
+   globalStats.RitmoGlobalStat = globalStats.RitmoGlobalStat + card.dataCard.RitmoScore;
+   globalStats.RitmoGlobalStat_TMP.text = "Poder Ritmico: " + globalStats.RitmoGlobalStat.ToString();
+
+    globalStats.ArmoniasGlobalStat = globalStats.ArmoniasGlobalStat + card.dataCard.ArmoniaScore;
+    globalStats.ArmoniasGlobalStat_TMP.text = "Poder Armonico: " + globalStats.ArmoniasGlobalStat.ToString();
+
+    globalStats.MelodiasGlobalStat = globalStats.MelodiasGlobalStat + card.dataCard.MelodiaScore; 
+    globalStats.MelodiasGlobalStat_TMP.text = "Poder Melodico: " + globalStats.MelodiasGlobalStat.ToString();
+  }
 
   public void OnPointerClick(PointerEventData eventData) { }
 
